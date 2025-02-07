8601cad3d1827fa3265b7e46dddfb6a6f1909f91
pandyamarut <pandyamarut@gmail.com>
2025-02-07 12:56:51 -0800

    add modepath util and cache
    Signed-off-by: pandyamarut <pandyamarut@gmail.com>

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,5 @@
 import json
-
+import os 
 
 def format_chunk(chunk):
     chunk = chunk.strip()
@@ -24,3 +24,21 @@ def process_response(response):
         if line:
             decoded_line = line.decode('utf-8')
             yield format_chunk(decoded_line)
+
+def topath(raw: str) -> str:
+    raw = raw.strip()
+    if ":" in raw:
+            model, branch = raw.rsplit(":", maxsplit=1)
+    else:
+            model, branch = raw, "main"
+    if "/" not in model:
+        raise ValueError(f"invalid model: expected one in the form user/model[:path], but got {model}")
+    user, model = model.rsplit("/", maxsplit=1)
+    return os.path.join("/runpod", "cache", "models", user, model, branch)
+
+
+def modelpaths() -> list[str]:
+    raw = os.environ.get("RUNPOD_HUGGINGFACE_MODEL")
+    if not raw:
+        return []
+    return [topath(m) for m in raw.split(",")]
